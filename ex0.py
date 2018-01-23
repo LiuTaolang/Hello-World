0111644fafa6754b122c534e93f31b248c6871a3
jayer <jayer.ltl@gmail.com>
2018-01-23 21:47:54 +0800

    Add files via upload
    注释里的序号对应 lpthw 里的 Windows 安装的步骤。

---
+++ ex0.py
@@ -0,0 +1,4 @@
+# 3. win + R, then Enter "powershell" to run powershell.
+# 6. create a "python36" folder at C disk root directory, then install python in this folder.
+# 7. if can't open python in Terminal, firstly enter command: [Environment]::SetEnvironmentVariable(“Path”,”$env:Path;C:\Python36″, “User”
+#                                      secondly close then restart Terminal, try again.
