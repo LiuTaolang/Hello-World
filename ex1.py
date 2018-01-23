b0e66395397515f58ff456b1efdcdb8750310035
jayer <jayer.ltl@gmail.com>
2018-01-23 22:06:23 +0800

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,9 @@
+print("Hello World！")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+# THe Study Drills contain things you should try to do. if you can't, skip it and come back later.
